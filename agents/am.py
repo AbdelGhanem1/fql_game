18540e95481243ffad12718489158c33f26f3102
abdelghani ghanem <ghanem96abdelghani@gmail.com>
2026-01-16 13:50:48 +0100

    next commit

--- agents/am.py
+++ agents/am.py
@@ -227,7 +227,7 @@ def get_config():
     # Placeholder config; values will be populated from FLAGS in main.py
     config = ml_collections.ConfigDict(dict(
         agent_name='adjoint_matching',
-        lr=5e-5,
+        lr=2e-5,
         batch_size=256,
         actor_hidden_dims=(512, 512, 512, 512),
         actor_layer_norm=False,
