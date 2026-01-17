7b5e81fc56288acbe9e5efe0019792e48b7c0575
abdelghani ghanem <ghanem96abdelghani@gmail.com>
2026-01-18 00:39:26 +0100

    next commit

--- agents/am.py
+++ agents/am.py
@@ -282,7 +282,7 @@ class AdjointMatchingAgent(flax.struct.PyTreeNode):
 def get_config():
     config = ml_collections.ConfigDict(dict(
         agent_name='adjoint_matching',
-        lr=3e-5,
+        lr=1e-5,
         batch_size=256,
         actor_hidden_dims=(512, 512, 512, 512),
         actor_layer_norm=False,
