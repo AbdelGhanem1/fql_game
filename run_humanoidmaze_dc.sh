423eba0453fe53395dc3bc534c367a45f02856f4
AbdelGhanem1 <ghanem96abdelghani@gmail.com>
2026-04-22 18:07:52 +0100

    adjustments for runpod

--- run_humanoidmaze_dc.sh
+++ run_humanoidmaze_dc.sh
@@ -83,6 +83,8 @@ fi
 cd "$PROJECT_DIR"
 mkdir -p /models/logs /models/saved_models
 
+export WANDB_MODE="offline"
+
 export WANDB_PROJECT="humanoidmaze-large_mirror_descent"
 export WANDB_NAME="task${TASK_ID}_tmp${INV_TEMP}_mprob${MIXTURE_PROB}_${SCORE_MODE}_dims${DIMS_TAG}_alpha${ME_AM_ALPHA}_tauC${TAU_CRITIC}_tauS${TAU_SCORE}_seed${SEED}"
 
